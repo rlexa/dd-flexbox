26b7bb3a982b08dc6c21eae33dddabc54b82656e
Alex Rempel <alexey.rempel@gmail.com>
2018-05-07 11:29:52 +0200

    fixing packager, add some to gitignore, fix win-install

--- win-install.bat
+++ win-install.bat
@@ -2,6 +2,6 @@ setlocal
 set SKIP_SASS_BINARY_DOWNLOAD_FOR_CI=1
 set SASS_BINARY_PATH=%~dp0sass-binaries\v4-9-0\win32-x64-57_binding.node
 
-ng update @angular/cli
-rem call npm install
+rem ng update @angular/cli
+call npm install
 call robocopy %~dp0sass-binaries\v4-9-0\vendor %~dp0node_modules\node-sass\vendor /s /e
