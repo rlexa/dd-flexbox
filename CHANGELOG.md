75a9830621c91b6973bd67d6d75796707a8c8bc9
Alex Rempel <alexey.rempel@gmail.com>
2018-05-07 10:55:24 +0200

    migrate to angular 6.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 0.10.0
+
+### Breaking Changes
+
+* **@angular:** now requires @angular 6.0.0 framework version
+
 # 0.9.2
 
 ### Added
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FlexboxModule } from './modules/flexbox/flexbox.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FlexboxModule } from './modules/flexbox/flexbox.module';
 
 @NgModule({
   declarations: [AppComponent],
